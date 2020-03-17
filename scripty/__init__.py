380594af327f48db15dad69a34d22f36a607662c
Alex Sales <alexander.sales@equinox.com>
2020-03-17 16:52:17 +0800

    DATPLAT-1711 add default from_date

--- scripty/__init__.py
+++ scripty/__init__.py
@@ -1,6 +1,3 @@
 import os
 
-DEFAULT_FROM_DATE = '1776-07-04'
-
-if os.environ['DEFAULT_FROM_DATE']:
-    DEFAULT_FROM_DATE = os.environ['DEFAULT_FROM_DATE']
+DEFAULT_FROM_DATE = os.environ.get('DEFAULT_FROM_DATE', '1776-07-04')
